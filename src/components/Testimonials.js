4ecf0c41b642fb69d36afc2814f9ff0e6f9da865
Abhik Das <abhikdas98765@gmail.com>
2021-07-04 14:19:48 +0530

    initial commit

---
+++ src/components/Testimonials.js
@@ -0,0 +1,146 @@
+import React from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
+import { IoMdCheckmarkCircleOutline } from "react-icons/io"
+import { FaRegLightbulb } from "react-icons/fa"
+import { graphql, useStaticQuery } from "gatsby"
+
+const Testimonials = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(
+        filter: {
+          ext: { regex: "/(jpg)|(png)|(jpeg)/" }
+          name: { in: ["testimonial1", "testimonial2"] }
+        }
+      ) {
+        edges {
+          node {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <TestimonialsContainer>
+      <TopLine>Testimonials</TopLine>
+      <Description>What people are saying</Description>
+      <ContentWrapper>
+        <ColumnOne>
+          <Testimonial>
+            <IoMdCheckmarkCircleOutline
+              css={`
+                color: #3fffa8;
+                font-size: 2rem;
+                margin-bottom: 1rem;
+              `}
+            />
+            <h3>Steve Mason</h3>
+            <p>
+              "Greatest experience of my life. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Excepturi accusantium aspernatur
+              accusamus iste inventore ratione."
+            </p>
+          </Testimonial>
+          <Testimonial>
+            <FaRegLightbulb
+              css={`
+                color: #f9b19b;
+                font-size: 2rem;
+                margin-bottom: 1rem;
+              `}
+            />
+            <h3>Sirisha Roy</h3>
+            <p>
+              "It was so easy to set up my trip. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Saepe voluptate, nobis dolore
+              officia quos blanditiis. "
+            </p>
+          </Testimonial>
+        </ColumnOne>
+        <ColumnTwo>
+          {data.allFile.edges.map((img, index) => (
+            <Image key={index} alt="testimonial" fluid={img.node.childImageSharp.fluid} />
+          ))}
+        </ColumnTwo>
+      </ContentWrapper>
+    </TestimonialsContainer>
+  )
+}
+
+export default Testimonials
+
+const TestimonialsContainer = styled.div`
+  width: 100%;
+  background: #fcfcfc;
+  color: #000;
+  padding: 5rem calc((100vw- 1300px) / 2);
+  height: 100%;
+`
+
+const TopLine = styled.p`
+  color: #077cf1;
+  font-size: 1rem;
+  padding-left: 2rem;
+  margin-bottom: 0.75rem;
+`
+
+const Description = styled.p`
+  text-align: start;
+  padding-left: 2rem;
+  margin-bottom: 4rem;
+  font-size: clamp(1.5rem, 5vw, 2rem);
+  font-weight: bold;
+`
+
+const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const ColumnOne = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+`
+
+const Testimonial = styled.div`
+  padding-top: 1rem;
+  padding-right: 2rem;
+
+  h3 {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+    font-style: italic;
+  }
+
+  p {
+    color: #3b3b3b;
+  }
+`
+
+const ColumnTwo = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 2rem;
+  grid-gap: 10px;
+
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const Image = styled(Img)`
+  border-radius: 10px;
+  height: 100%;
+`
